071445c34371317f4907ff357a738ebd8588eb33
Alejandro Paredero <alejandro@paredero.com>
2025-01-28 13:50:25 +0100

    Translated variables, added info message with link to repo

--- app.py
+++ app.py
@@ -2,38 +2,46 @@ import requests
 import streamlit as st
 import pandas as pd
 
-#Initializing variable for certificate validation
-cert_valido = True
+# Initializing variable for certificate validation
+valid_cert = True
+
+# Streamlit page configuration
+st.set_page_config(
+    page_title="IP geolocation",
+    page_icon="world_map",
+    layout="wide",
+    )
 
 # Streamlit app title
-st.header("IP geolocation", divider=True)
+st.info("Source code of this example can be found on: https://github.com/aparedero/streamlit-geolocation-example")
+st.header("IP geolocation 🗺️", divider=True)
 
 # Streamlit sidebar configuration parameters
 st.sidebar.subheader("Settings")
-protocolo = st.sidebar.radio("Select API protocol to request at ifconfig.me:", ["HTTP", "HTTPS"])
-if protocolo == "HTTPS":
-    cert_valido = st.sidebar.checkbox("Validate certficate", value=True)
+protocol = st.sidebar.radio("Select API protocol to request at ifconfig.me:", ["HTTP", "HTTPS"])
+if protocol == "HTTPS":
+    valid_cert = st.sidebar.checkbox("Validate certficate", value=True)
 
 #Python requests to get the IP address and location
-direccion_ip = requests.get(f"{protocolo.lower()}://ifconfig.me", verify=cert_valido)
-localizacion = requests.get(f"http://ip-api.com/json/{direccion_ip.text}", verify=cert_valido)
+ip_address = requests.get(f"{protocol.lower()}://ifconfig.me", verify=valid_cert)
+location = requests.get(f"http://ip-api.com/json/{ip_address.text}", verify=valid_cert)
 
 #Show IP address
-st.write(f"**Your IP address is:** {direccion_ip.text}")    
+st.write(f"**Your IP address is:** {ip_address.text}")    
 
 #Show capture information as dataframe
 st.write("Dataframe")
-df2 = pd.DataFrame([localizacion.json()])
+df2 = pd.DataFrame([location.json()])
 st.dataframe(df2) 
 
 #Create map section
 st.subheader("Map 🌍")
 
 # Show captured city and create map object
-st.write(f"**Your city is:** {localizacion.json()['city']}")
-df = pd.DataFrame({"lat": [localizacion.json()['lat']], "lon": [localizacion.json()['lon']] })
+st.write(f"**Your city is:** {location.json()['city']}")
+df = pd.DataFrame({"lat": [location.json()['lat']], "lon": [location.json()['lon']] })
 st.map(df)
 
 # Show json contents as expander
 with st.expander("See JSON content from ip-api.com"):
-    st.write(localizacion.json())
+    st.write(location.json())
